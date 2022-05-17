d51288a7c02abcef051eee096e4161086cd37116
Justin James <justinmjames@protonmail.com>
2022-05-16 17:53:56 -0700

    commit tree e35b37e55f10bb2372ac09712d56a4c5cbe78041

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceFeet) {
+  if (distanceFeet <= 400) {
+    return 'This one is on me!';
+  } else if (distanceFeet > 400 && distanceFeet < 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distanceFeet > 2000 && distanceFeet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distanceFeet > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount) {
+  switch (tipAmount) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  };
 }
